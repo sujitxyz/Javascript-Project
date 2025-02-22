23648009d5c1ba24c44b3cd72b7518237845fc14
sujitxyz <bhusalsujit01@gmail.com>
2025-02-22 22:19:48 +0545

    js part of project is added

---
+++ project-files/time-date/main.js
@@ -0,0 +1,45 @@
+let dateContainer = document.querySelector(".date-container");
+let hoursContainer = document.querySelector(".hours");
+let minutesContainer = document.querySelector(".minutes");
+let secondsContainer = document.querySelector(".seconds");
+
+
+const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+const monthName =[
+    "January", "February", "March", "April", "May", "June", 
+  "July", "August", "September", "October", "November", "December"
+]; 
+
+function formatTime(time){
+    return time < 10 ? "0" + time : time;
+}
+
+function updateClock(){
+    const today = new Date();
+    // console.log(today);
+    let date = today.getDate();
+      console.log(date);
+      
+    let day = weekDays[today.getDay()];
+    let month = monthName[today.getMonth()];
+
+    let hours = formatTime(today.getHours());
+    let minutes = formatTime(today.getMinutes());
+    let seconds = formatTime(today.getSeconds());
+
+    dateContainer.innerHTML = `<p>${day}</p><p><span>${date}</span></p><p>${month}</p>`;
+
+    hoursContainer.textContent = hours + ":";
+    minutesContainer.textContent = minutes + ":";
+    secondsContainer.textContent = seconds;
+}
+setInterval(updateClock,1000);
